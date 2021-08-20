4ed6dd91d5141a76eef74ecb7a3f280ebf037363
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-08-20 10:55:06 +0530

    added buttons for themes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,21 @@ const Navbar = (props) => {
           <a className="nav-link" href="/">{props.aboutText}</a>
         </li>
       </ul>
-      <button type="button" class="btn btn-sm mx-2 my-1 text-light" style={{backgroundColor: "#0d5408", display: props.mode==="light"?"none":"block"}}>Green</button>
-      <button type="button" class="btn btn-sm mx-2 my-1 text-light" style={{backgroundColor: "#220154", display: props.mode==="light"?"none":"block"}}>Purple</button>
-      <button type="button" class="btn btn-sm mx-2 my-1 text-light" style={{backgroundColor: "#3d0047", display: props.mode==="light"?"none":"block"}}>Magenta</button>
+      {/* <button type="button" className="btn btn-sm mx-2 my-1 text-light" style={{backgroundColor: "#0d5408", display: props.mode==="light"?"none":"block"}}>Green</button>
+      <button type="button" className="btn btn-sm mx-2 my-1 text-light" style={{backgroundColor: "#220154", display: props.mode==="light"?"none":"block"}}>Purple</button>
+      <button type="button" className="btn btn-sm mx-2 my-1 text-light" style={{backgroundColor: "#3d0047", display: props.mode==="light"?"none":"block"}}>Magenta</button> */}
+      <div className="form-check form-switch mx-3 my-1">
+        <input className="form-check-input" style={{backgroundColor: "#138a1b", display: props.mode==="light"?"none":"block"}} type="checkbox" id="flexSwitchCheckDefault"/>
+        <label className="form-check-label" style={{color: "rgb(235 235 235)", display: props.mode==="light"?"none":"block"}} htmlFor="flexSwitchCheckDefault">Green Theme</label>
+      </div>
+      <div className="form-check form-switch mx-3 my-1">
+        <input className="form-check-input" style={{backgroundColor: "#6600eb", display: props.mode==="light"?"none":"block"}} type="checkbox" id="flexSwitchCheckDefault"/>
+        <label className="form-check-label" style={{color: "rgb(235 235 235)", display: props.mode==="light"?"none":"block"}} htmlFor="flexSwitchCheckDefault" >Purple Theme</label>
+      </div>
+      <div className="form-check form-switch mx-3 my-1">
+        <input className="form-check-input" style={{backgroundColor: "#b700d4", display: props.mode==="light"?"none":"block"}} type="checkbox" id="flexSwitchCheckDefault"/>
+        <label className="form-check-label" style={{color: "rgb(235 235 235)", display: props.mode==="light"?"none":"block"}} htmlFor="flexSwitchCheckDefault">Magenta Theme</label>
+      </div>
       <div className="form-check form-switch">
       <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
       <label className={`form-check-label text-${props.mode==="light"?"dark":"light"}`} htmlFor="flexSwitchCheckDefault">Enable {props.mode==="light"?"Dark":"Light"} mode</label>
